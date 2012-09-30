55b80aebdf7dde7a727eced8ad69f1a9f8c6577c
BM HOMER <bmh10@ic.ac.uk>
2012-09-30 08:13:21 +0100

    test

--- src/game/Game.java
+++ src/game/Game.java
@@ -298,7 +298,7 @@ public class Game extends Applet implements Runnable {
 					startNewGame();
 				}
 				else if (rR.contains(selCoin.getPos().x, selCoin.getPos().y))
-				{redgdgdg
+				{redgdg
 					//Goal to left player
 					scores[0]++;
 					startNewGame();
